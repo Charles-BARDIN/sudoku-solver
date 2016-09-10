586bebeec1a62ce9db3c11634472156704ae7520
Charles BARDIN <bardincharles@gmail.com>
2016-09-10 18:16:12 +0200

    update demo

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ const sudoku = require('.');
 const Grid = sudoku.Grid;
 const Solver = sudoku.Solver;
 
-let pattern = '..53.....8......2..7..1.5..4....53...1..7...6..32...8..6.5....9..4....3......97..';
+let pattern = '8..........36......7..9.2...5...7.......457.....1...3...1....68..85...1..9....4..';
 let grid = new Grid(pattern);
 let solver = new Solver();
 
